11da9bdb1f30f2bf14184eab37452e0b8f848927
annastepanova <annastepanova1811@gmail.com>
2020-07-02 19:46:49 -0400

    implementing JWT authentication

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User = require('../../models/user')
 
 module.exports = {
@@ -14,8 +17,42 @@ module.exports = {
     }
   },
   Mutation: {
-    register(_, args, context, info) {
-      
+    async register(_, { registerInput: {
+      name,
+      username,
+      email,
+      password,
+      confirmPassword
+    }
+  }, 
+  context, info) {
+      // hashing a password
+      password = await bcrypt.hash(password, 12)
+
+      const newUser = new User({
+        name,
+        username,
+        email,
+        password
+      })
+
+      //saving new User in DB
+      const res = await newUser.save()
+
+      // creating JSON token
+      const token = jwt.sign({
+        id: res.id,
+        name: res.name,
+        username: res.username,
+        email: res.email
+      }, process.env.JWT_KEY,
+        { expiresIn: '1hr' })
+
+        return {
+          ...res._doc,
+          id: res._id,
+          token
+        }
     }
   }
 }
