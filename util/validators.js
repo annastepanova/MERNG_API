5358a90f6e746b30575be383ace7adfbe9764558
annastepanova <annastepanova1811@gmail.com>
2020-07-02 20:42:42 -0400

    custom user input validation

---
+++ util/validators.js
@@ -0,0 +1,48 @@
+module.exports.validateRegisterInput = (
+  name,
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {}
+  if (name.trim() === '') {
+    errors.name = 'Name can not be empty'
+  }
+  if (username.trim() === '') {
+    errors.username = 'Username can not be empty'
+  }
+  if (email.trim() === '') {
+    errors.email = 'Email can not be empty'
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+    if (!email.match(regEx)) {
+      errors.email = 'Email should be a valid email address'
+    }
+  }
+  if (password === '') {
+    errors.password = 'Password can not be empty'
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = 'Passwords do not match'
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  }
+}
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {}
+  if (username.trim() === '') {
+    errors.username = 'Username can not be empty'
+  }
+  if (password.trim() === '') {
+    errors.password = 'Password can not be empty'
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
+  }
+}
