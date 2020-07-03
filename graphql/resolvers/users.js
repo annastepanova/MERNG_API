a626a5878faedc597a5d8cefe934ec6dc4e693aa
annastepanova <annastepanova1811@gmail.com>
2020-07-03 08:40:26 -0400

    login mutation resolver

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,8 +2,21 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { UserInputError } = require('apollo-server')
 const { validateRegisterInput } = require('../../util/validators')
+const { validateLoginInput } = require('../../util/validators')
 
 const User = require('../../models/user')
+
+ // creating JSON token
+const generateToken = (user) => {
+  return jwt.sign({
+    id: user.id,
+    name: user.name,
+    username: user.username,
+    email: user.email
+  }, process.env.JWT_KEY,
+    { expiresIn: '1hr'}
+  )
+}
 
 module.exports = {
   Query: {
@@ -19,6 +32,7 @@ module.exports = {
     }
   },
   Mutation: {
+    
     async register(_, { registerInput: {
       name,
       username,
@@ -74,20 +88,43 @@ module.exports = {
       //saving new User in DB
       const res = await newUser.save()
 
-      // creating JSON token
-      const token = jwt.sign({
-        id: res.id,
-        name: res.name,
-        username: res.username,
-        email: res.email
-      }, process.env.JWT_KEY,
-        { expiresIn: '1hr' })
+      const token = generateToken(res)
 
         return {
           ...res._doc,
           id: res._id,
           token
         }
+    },
+
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password)
+
+      if(!valid) {
+        throw new UserInputError('Invalid inputs passed, please check your data', { errors })
+      }
+
+      const user = await User.findOne({ username })
+
+      if (!user) {
+        errors.general = 'User not found'
+        throw new UserInputError('User not found', { errors })
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password)
+
+      if (!isValidPassword) {
+        errors.general = 'Invalid credentials, could not log you in'
+        throw new UserInputError('Invalid credentials', { errors })
+      }
+
+      const token = generateToken(user)
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token
+      }
     }
   }
 }
